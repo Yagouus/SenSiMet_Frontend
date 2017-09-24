ed41bf95ee2103976f5770801eec3c772df23cb4
Yagouus <yagofontenla95@gmail.com>
2017-09-24 22:24:06 +0200

    citius routing

--- scripts/services/restService.js
+++ scripts/services/restService.js
@@ -8,8 +8,8 @@ angular.module("project").service("restService", ["$http", function($http){
         return $http.get("https://babelnet.io/v4/getSynset?id=" + synsetID + "&key=1e545625-7f8d-44be-a678-140b25965229")
     };
 
-    this.url = 'http://localhost:8080/';
-    //this.url = 'https://tec.citius.usc.es/sensimet-backend/'
+    //this.url = 'http://localhost:8080/';
+    this.url = 'https://tec.citius.usc.es/sensimet-backend/'
     //this.url = 'http://172.16.244.168:8081'
 
 }]);
